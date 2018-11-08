d87a1362f6ec5f6f3f67c3c3ca8d2796e3371662
Ian Aranha <ianorama@gmail.com>
2018-11-08 10:46:43 -0800

    Use destructuring assignment to assign variable from array

---
+++ E6Functions8.js
@@ -0,0 +1,17 @@
+// Use destructuring assignment to swap the values of a and b so 
+// that a receives the value stored in b, and b receives the value stored in a.
+
+let a = 8, b = 6;
+
+(() => {
+  "use strict";
+  // change code below this line
+  let x = a
+  a = b
+  b = x;
+  // change code above this line
+})();
+
+
+console.log(a); // should be 6
+console.log(b); // should be 8
